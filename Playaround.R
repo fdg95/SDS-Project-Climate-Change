91c0ce5ae0a6a4883ac9c782d7948df52a4ddc3e
fdg95 <64794827+fdg95@users.noreply.github.com>
2021-03-18 10:10:49 +0100

    Add files via upload

--- Playaround.R
+++ Playaround.R
@@ -45,24 +45,13 @@ timeline_filtered = timeline_db[rows, ] %>% head(200)#Reshuffle dataframe and on
 timeline_filtered = timeline_filtered %>% select(created_at, screen_name,followers_count, retweet_count, text) #Only keep relevant columns
 write_as_csv(timeline_filtered, "timeline_filtered.csv")
 
-
-
-#### Section: Adding UP ####
-
-#Adding Sentiment to the data for better checking
-data$vader = vaderdf$compound
-data$syudf = syudf
-write_as_csv(data, "data.csv")
-
-#### Section: Loading the data ####
+#### Section: Sentiment with World Libraries ####
+#THIS SECTION IS NOT USED
 
 df = read.csv("data.csv")
 
 #Add an id column
 df$id = seq.int(nrow(df))
-
-
-#### Section: Sentiment with World Libraries ####
 
 #Store text and create tible
 texts = df$text
@@ -112,8 +101,7 @@ df1$Sentiment1[select] = NA
 select = df1$Rel2 == 0
 df1$Sentiment2[select] = NA
 
-#Find sentiment of labelers
-
+#For all 4 labelers get a distribution of what they labeled (Positive, Negative, Neutral, Non-relevant)
 sent1_1 = df1 %>% group_by(Sentiment1) %>% summarise(number = n())
 sent1_2 = df1 %>% group_by(Sentiment2) %>% summarise(number = n())
 sent2_1 = df2 %>% group_by(Sentiment1) %>% summarise(number = n())
@@ -123,30 +111,47 @@ sent2_2 = df2 %>% group_by(Sentiment2) %>% summarise(number = n())
 eval = data.frame(sent1_1$number, sent1_2$number, sent2_1$number, sent2_2$number)
 non_relevant = eval[4,] / nrow(df1)
 relevant = 1 -(eval[4,] /(nrow(df1)))
-negative = eval[1,] / (100 * first_column)
-neutral = eval[2,] / (100 * first_column)
-positive = eval[3,] / (100 * first_column)
 
+#If set to 'all', calculates positive, negative and neutral percentage wrt to all labeled tweets
+#Otherwise wrt to only the relevant tweets
+set_total = 'rel'
+
+if(set_total == 'all'){
+  negative = eval[1,] / (100)
+  neutral = eval[2,] / (100)
+  positive = eval[3,] / (100)
+} else {
+  negative = eval[1,] / (100 * relevant)
+  neutral = eval[2,] / (100 * relevant)
+  positive = eval[3,] / (100 * relevant)
+} 
+  
+#Combine results into single df
 analysis_labeling = rbind(relevant, negative, neutral, positive, non_relevant)
-
-analysis_labeling = as_tibble(t(new))
+analysis_labeling = as_tibble(t(analysis_labeling))
 colnames(analysis_labeling) = c("relevant", "negative", "neutral", "positive", "non_relevant")
 
+
+#Check agreement between regarding relevance of the tweets
 df1$agree = (df1$Rel1 == df1$Rel2)
-agree1 = df1 %>% group_by(agree) %>% summarise(number = n())
+agreement_relevance1 = df1 %>% group_by(agree) %>% summarise(number = n())
 
 df2$agree = (df2$Rel1 == df2$Rel2)
-agree2 = df2 %>% group_by(agree) %>% summarise(number = n())
+agreement_relevance2 = df2 %>% group_by(agree) %>% summarise(number = n())
+
+
+#Check agreement regarding sentiment and relevance
+df1[is.na(df1)] = -5 #Set all NA values to -5, such that the == operator does not evaluate to NA
+df2[is.na(df2)] = -5
 
 df1$agree = (df1$Sentiment1 == df1$Sentiment2)
-agree11 = df1 %>% group_by(agree) %>% summarise(number = n())
+agreement_overall1 = df1 %>% group_by(agree) %>% summarise(number = n())
 
 df2$agree = (df2$Sentiment1 == df2$Sentiment2)
-agree22 = df2 %>% group_by(agree) %>% summarise(number = n())
+agreement_overall2 = df2 %>% group_by(agree) %>% summarise(number = n())
 
 
-
-#### Section: Sentiment Classification ####
+#### Section: Sentiment Classification Trial Set ####
 
 #Read in the annotated csv file for classification
 df = read.csv("timeline_filtered_anno.csv",
@@ -155,6 +160,11 @@ df = read.csv("timeline_filtered_anno.csv",
 #Analyse the tweets with vader 
 vaderdf = vader_df(df$text)
 df$vader = vaderdf$compound
+df$pos = vaderdf$pos
+df$neg = vaderdf$neg
+df$neu = vaderdf$neu
+
+write_as_csv(df, 'timeline_filtered_classified_test.csv')
 
 #Analyze the tweets with syuzhet
 syudf = get_sentiment(df$text)
@@ -189,3 +199,40 @@ print(conf1)
 write_as_csv(df, 'timeline_filtered_classified.csv')
 
 
+#### Section: Sentiment Classification Full Set ####
+
+#Import all tweets extracted and at least 1 week old at the time
+df = read.csv("timeline_db.csv")
+
+#Classify tweets according to vader and add precise score to df
+#dfvader = vader_df(df$text)
+#Store vader classification (takes ~20 min to run)
+#write_as_csv(dfvader, 'vader_classification_timeline_db.csv')
+
+dfvader = read.csv('vader_classification_timeline_db.csv')
+df$vader = dfvader$compound
+
+#Set threshold (same as in trial set)
+positive_threshold = 0.8
+negative_threshold = -0.3
+
+#Set labels according to threshold
+df$threshold = 0
+
+df$threshold[df$vader >= positive_threshold] = 1
+df$threshold[df$vader <= negative_threshold] = -1
+
+#Get Insights
+#Numbers of tweets classified as positive (937), neutral (5867) and negative(1118)
+df %>% group_by(threshold) %>% summarise(count = n())
+
+#Select 100 most positive and most negative tweets
+df_positive = df %>% arrange(desc(vader)) %>% head(100)
+df_negative = df %>% arrange(vader) %>% head(100)
+
+#Save all the dataframes
+write_as_csv(df, 'timeline_db_classified.csv')
+write_as_csv(df_positive, 'timeline_db_positive.csv')
+write_as_csv(df_negative, 'timeline_db_negative.csv')
+
+#### Section: Graphs and Results ####
